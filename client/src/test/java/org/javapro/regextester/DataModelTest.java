9557e87ebc31c37e75c67be1dc9adae29e4d0035
javatlacati <scherzo_16@hotmail.com>
2018-12-26 07:29:42 -0600

    testing generation calls on General client code

--- client/src/test/java/org/javapro/regextester/DataModelTest.java
+++ client/src/test/java/org/javapro/regextester/DataModelTest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Set;
+import org.javapro.regextester.js.PlatformServices;
 
 import static org.junit.Assert.*;
 
@@ -16,25 +18,63 @@ import static org.junit.Assert.*;
 @RunWith(BrowserRunner.class)
 public class DataModelTest {
     
+    private class MockedPLatformServices extends PlatformServices{
+
+        @Override
+        public Set<String> nPossibilities(String regexText) {
+            return null; //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    @Test
+    public void testNoGeneratedPossibilities() {
+        RegexTesting model = new RegexTesting("", "", "", false, false, false, false);
+        PlatformServices mockedServices =new MockedPLatformServices();
+        model.setPreferences(mockedServices);
+        DataModel dm= new DataModel();
+        dm.setPreferences(model, mockedServices);
+        dm.nPossibilities(model);
+        assertEquals(0, model.getPossibilities().size());
+        assertEquals(mockedServices, dm.getServices());
+        assertTrue(model.isMatches());
+    }
+
     @Test
     public void testEscaping() {
         RegexTesting model = new RegexTesting("f.o,bar,hello,world", "f\\.o,(bar)", "$1", false, false, false, false);
         model.setTestCase("123");
         model.setReplacementText("");
         DataModel.setEscapedRegexText(model, "\\\\d+");
-        assertEquals("\\d+",model.getRegexText());
+        assertEquals("\\d+", model.getRegexText());
         assertTrue(model.isMatches());
     }
-    
+
+    /**
+     * Tests error message displaying when replacement is requested for non existing group.
+     * <br>
+     * Input: <code>f.o,bar,hello,world</code><br>
+     * Regex: <code>\d+</code><br>
+     * Replacement: <code>$1</code><br>
+     */
     @Test
     public void testFailedEscaping() {
         RegexTesting model = new RegexTesting("f.o,bar,hello,world", "f\\.o,(bar)", "$1", false, false, false, false);
         model.setTestCase("123");
         DataModel.setEscapedRegexText(model, "\\\\d+");
-        assertEquals("No group 1",model.getRegexText());
+        assertEquals("No group 1", model.getRegexText());
         assertFalse(model.isMatches());
     }
 
+    /**
+     * Tests capture groups for searching.
+     * <br>
+     * Input: <code>f.o,bar,hello,world</code><br>
+     * Regex: <code>f\.o,(bar)</code><br>
+     * Replacement: <code>$1</code><br>
+     * Expected replacement result: <code>bar,hello,world</code><br>
+     * Capture groups matched: <code>[bar]</code>
+     */
     @Test
     public void testCapturingGroup() {
         RegexTesting model = new RegexTesting("f.o,bar,hello,world", "f\\.o,(bar)", "$1", false, false, false, false);
@@ -53,6 +93,14 @@ public class DataModelTest {
         assertEquals(Collections.singletonList("jim"), model.getGroupsMatching());
     }
 
+    /**
+     * Tests Unicode blocks in replacement.
+     * <br>
+     * Input: <code>abcdefghijkTYYtyyQ</code><br>
+     * Regex: <code>(\p{Lu})</code><br>
+     * Replacement: <code>_</code><br>
+     * Expected replacement result: <code>abcdefghijk___tyy_</code>
+     */
     @Test
     public void testUnicode() {
         RegexTesting model = new RegexTesting("abcdefghijkTYYtyyQ", "(\\p{Lu})", "_", true, false, false, false);
@@ -60,9 +108,17 @@ public class DataModelTest {
         assertEquals("(\\\\p{Lu})", DataModel.escapedRegexText(model.getRegexText()));
         assertEquals("abcdefghijk___tyy_", model.getReplaced());
         DataModel.allGroups(model);
-        assertEquals(Arrays.asList("T","Y","Y","Q"), model.getGroupsMatching());
+        assertEquals(Arrays.asList("T", "Y", "Y", "Q"), model.getGroupsMatching());
     }
 
+    /**
+     * Tests look ahead in replacement.
+     * <br>
+     * Input: <code>abcdefghijkTYYtyyQ</code><br>
+     * Regex: <code>(?=\p{Lu})</code><br>
+     * Replacement: <code>_</code><br>
+     * Expected replacement result: <code>abcdefghijk_T_Y_Ytyy_Q</code>
+     */
     @Test
     public void testUnicodeLookahead() {
         RegexTesting model = new RegexTesting("abcdefghijkTYYtyyQ", "(?=\\p{Lu})", "_", true, false, false, false);
